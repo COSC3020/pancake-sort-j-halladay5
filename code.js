757758bb3d98a2dae2fcf6aa30823abbc96e2868
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-03-17 20:45:00 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,46 @@
-function flip(array, n) {
+function pancakesort(array)
+{
+    var flipPos;
+    
+    if(array.length < 2)
+    {
+        return array;
+    }
+    
+    for(var i = 1; i < array.length; i++)
+    {
+        flipPos = 0;
+        if(array[i] < array[0])
+        {
+            flip(array, i);
+            flip(array, i+1);
+        }
+        
+       if((array[i] > array[0]) && (array[i] < array[i-1]))
+       {
+           flip(array, i+1);
+           while(array[0] <= array[flipPos])
+           {
+               flipPos++;
+           }
+           flip(array, flipPos);
+           flip(array, flipPos-1);
+           flip(array, i+1);
+       }
+    }
     return array;
 }
 
-// Use only flip() here to manipulate the array
-function pancakeSort(array) {
+function flip(array, n)
+{
+    let temp = array.slice(0, n);
+    temp.reverse();
+    
+    for(let i = 0; i < n; i++)
+    {
+        array[i] = temp[i];
+    }
+    
     return array;
 }
+
